cb5465ec6fe9358d52830a3878f5bbea35395c7d
AYousry12 <es.ahmed.yousry2023@alexu.edu.eg>
2026-03-04 14:26:59 +0200

    here we go

---
+++ src/main/java/com/example/sorting_project/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.sorting_project;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
